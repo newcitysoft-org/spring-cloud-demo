5ea1af6c2dc808648c6324727400b5b7b244f59b
tianlixin <newcitysoft@163.com>
2018-04-24 15:46:51 +0800

    增加Hystrix监控数据聚合监控面板项目模块。

---
+++ README.md
@@ -0,0 +1,12 @@
+## Spring Cloud Demo
+### 学习地址
+* [SpringCloud-Learning](https://github.com/dyc87112/SpringCloud-Learning)
+
+* [spring-cloud-examples](https://github.com/ityouknow/spring-cloud-examples)
+
+* []()
+
+
+### Spring Cloud 案例架构图
+
+![](http://blog.didispace.com/content/images/posts/spring-cloud-starter-dalston-5-2-1.png)
---
+++ turbine/src/main/java/com/newcitysoft/research/TurbineApplication.java
@@ -0,0 +1,22 @@
+package com.newcitysoft.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/4/24 15:36
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+}
