4bb4ca3e607ef89c73ce18ff5b320728bc1a87d2
tianlixin <newcitysoft@163.com>
2018-04-24 15:03:17 +0800

    增加Hystrix监控面板项目模块。

---
+++ hystrix-dashboard/src/main/java/com/newcitysoft/research/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.newcitysoft.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/4/24 14:55
+ */
+@EnableHystrixDashboard
+@SpringCloudApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
